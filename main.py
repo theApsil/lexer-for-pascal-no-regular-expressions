faa02b29518b07209b33d96fd6be554b2c618d14
Apsil <tumbler.m.r1@gmail.com>
2023-06-13 22:11:05 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from lexer import Lexer, Token
+from parser import Parser
 
+f = open("code.pas")
 
-with open('code.pas', 'r') as file:
-    input_code = file.read()
-
-lexer = Lexer(input_code)
-token = Token(0, 0, '', '')
-while True:
-    token = lexer.get_next_token()
-    if token.value == 'end':
-        previousToken = token
-        token = lexer.get_next_token()
-        if previousToken.value == 'end' and token.value == '.':
-            print(previousToken)
-            print(token)
-            break
-    if token.type == Lexer.EOF:
-        break
+lexer = Lexer(f)
+token = lexer.get_next_token()
+while token.name != Token.EOF:
     print(token)
+    token = lexer.get_next_token()
+print(token)
+f.close()
+
+
